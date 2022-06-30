f15e8d33ba16a0a28927a34f5d379b9460eed092
Michael Gearhardt <michael.gearhardt@fathym.com>
2022-06-30 17:54:49 -0600

    new LCUSTateAPIs base

---
+++ LCU.Personas.StateAPI/LCUStateAPIs.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace LCU.Personas.StateAPI
+{
+    public class LCUStateAPIs<TStateEntity> : LCUStateAPI
+    {
+        #region Constructors
+        public LCUStateAPIs(ILogger logger)
+            : base(logger)
+        { }
+        #endregion
+
+        #region Helpers
+        protected virtual async Task signalStateShare(IDurableEntityClient client, IAsyncCollector<SignalRMessage> signalRMessages,
+            string stateKey)
+        {
+            var entityId = new EntityId(typeof(TStateEntity).Name, stateKey);
+
+            var state = await client.ReadEntityStateAsync<EaCStateEntity>(entityId);
+
+            await signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    GroupName = stateKey,
+                    Target = "state-update",
+                    Arguments = new[] { state.EntityState }
+                });
+        }
+        #endregion
+    }
+}

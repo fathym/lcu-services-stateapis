b2e007e4fd43f127092ffe3b00e96fffdac3bdea
Michael Gearhardt <michael.gearhardt@fathym.com>
2022-09-08 17:57:06 -0600

    ObjectExtension for ToJToken()

---
+++ Fathym.LCU.Services.StateAPIs/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Fathym.LCU.Services.StateAPIs
+{
+    public static class ObjectExtensions
+    {
+        public static Newtonsoft.Json.Linq.JToken ToJToken(this object obj)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JToken>(obj.ToJSON());
+        }
+    }
+
+}
